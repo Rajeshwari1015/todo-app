9cc336a5412bb7fb2da9b37d913fa1e7193e91d1
Rajeshwari1015 <rajeshwari1503aarthy@gmail.com>
2026-05-01 22:16:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") return;
+
+  let li = document.createElement("li");
+  li.innerHTML = `
+    ${task}
+    <button onclick="deleteTask(this)">X</button>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
+
+function deleteTask(btn) {
+  btn.parentElement.remove();
+}
